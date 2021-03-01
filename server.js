ad2e3ff009664765155e547cd5cf0a1b9b517125
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-03-01 21:31:00 +0545

    new

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const connectDB = require('./server/database/connection');
 const app = express();
 
-const port = process.env.PORT ||3000;
+const port = process.env.PORT ||port;
 
 dotenv.config({path:'config.env'});
 
