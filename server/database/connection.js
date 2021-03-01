4da6884087e0a9f0426202fd6e9edc4d668a7f79
Mukesh Mahara <mukeshmahara1@gmail.com>
2021-03-01 21:23:00 +0545

    new

--- server/database/connection.js
+++ server/database/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB  = async()=>{
     try {
         //MongoDB connection String
-        const con =await mongoose.connect(process.env.PORT,{
+        const con =await mongoose.connect(process.env.MONGO_URI,{
             useNewUrlParser:true,
             useUnifiedTopology:true,
             useFindAndModify:false,
